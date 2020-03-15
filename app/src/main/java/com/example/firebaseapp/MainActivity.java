c465042b38331a2691f6e66b0450258a102433bc
Anni1123 <kumarianni1213@gmail.com>
2020-03-15 23:22:15 +0530

    ForAnnouncement

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -8,33 +8,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button sendData;
-    private EditText adddata;
-    private EditText key;
-    private Firebase mroorRef;
+    private TextView adddata;
 
+    private Firebase mref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mroorRef=new Firebase("https://fir-app-d2e04.firebaseio.com/Users");
-        sendData=(Button)findViewById(R.id.addBtn);
-        adddata=(EditText) findViewById(R.id.valueField);
-        key=(EditText)findViewById(R.id.keyValue);
-        sendData.setOnClickListener(new View.OnClickListener() {
+
+        adddata=(TextView) findViewById(R.id.keyValue);
+        mref=new Firebase("https://fir-app-d2e04.firebaseio.com/");
+        mref.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                String data=adddata.getText().toString();
-                String keyVal=key.getText().toString();
-                Firebase childref=mroorRef.child(keyVal);
-                //childref.setValue(data);
-                childref.setValue(data);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value=dataSnapshot.getValue().toString();
+                adddata.setText(value);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
             }
         });
-
     }
 }
