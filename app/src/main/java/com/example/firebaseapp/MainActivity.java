1d1ef2470032ee82e4ed08e4168b9b2f007e782b
Anni1123 <kumarianni1213@gmail.com>
2020-03-15 23:41:56 +0530

    FOrUpdatingInListFOrm

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -4,32 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView adddata;
-
+    private ListView listView;
+    private ArrayList musename=new ArrayList<>();
     private Firebase mref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adddata=(TextView) findViewById(R.id.keyValue);
-        mref=new Firebase("https://fir-app-d2e04.firebaseio.com/");
-        mref.addValueEventListener(new ValueEventListener() {
+        listView=(ListView)findViewById(R.id.list);
+        mref=new Firebase("https://fir-app-d2e04.firebaseio.com/Users");
+        final ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,musename);
+        listView.setAdapter(arrayAdapter);
+        mref.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 String value=dataSnapshot.getValue().toString();
-                adddata.setText(value);
+                musename.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
