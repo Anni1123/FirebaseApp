a5e4148109f1f276f1e75e5e8370e07bba80a068
Anni1123 <kumarianni1213@gmail.com>
2020-03-15 22:13:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.firebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button sendData;
+    private Firebase mref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        mref=new Firebase("https://fir-app-d2e04.firebaseio.com/");
+        sendData=(Button)findViewById(R.id.addData);
+        sendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
